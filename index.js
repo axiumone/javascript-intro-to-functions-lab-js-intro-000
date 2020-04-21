fbb9acdb6cb0b0f56a27e9853f6755785de67e9d
Dmitry Sychev <axiumone@gmail.com>
2020-04-21 19:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  if(string.toLowerCase) {
-  return "I can\'t hear you";
-} else {
-  return "YES INDEED!";
-  }
+function sayHiToGrandma(str) {
+   if (str.toLowerCase()===str) {
+   return "I can't hear you";
+ } else if (str.toUpperCase()===str) {
+   return "YES INDEED!";
+ } else if (str === "I love you, Grandma") {
+   return "I love you, too."
+ }
 }
