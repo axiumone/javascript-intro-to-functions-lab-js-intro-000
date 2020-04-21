28d785e0a22c814d994bbcd7bb41fae9bccf5cff
Dmitry Sychev <axiumone@gmail.com>
2020-04-21 19:58:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase() // "HELLO!"
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase()) // "HELLO!"
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase) {
+  return "I can\'t hear you";
+} else {
+  return "YES INDEED!";
+  }
+}
